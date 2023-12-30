d465c98de3fd1979884e4cff83785cf04890d225
patricy <patricysilvasantos@gmail.com>
2023-12-29 23:12:44 -0300

    q marmota foi essa

--- app/Http/Controllers/test.php
+++ app/Http/Controllers/test.php
@@ -8,24 +8,27 @@ class test extends Controller
 {
     public function index()
     {
-       $livros = [
-        "dia e noite",
-        "declinio",
-        "rua do medo"
-    ];
-       
-    $monica = "dentuça";
-    $cebolinha = "egoista";
-    $cascao = "sujo";
-    $magali = "gulosa";
- $turmamonica= [
-            "monica: $monica",
-            "cascao:$cascao ",
-            "cebolinha:$cebolinha ",
-            "magali:$magali "
+        date_default_timezone_set('America/Sao_Paulo');
+        $agora=date('H:i');
+        $agora=date('17:58');
+      
+        $noite_inicio=('18:00');
+        $noite_fim=('00:00');
 
-        ];
-    print_r($turmamonica);
+        $manha_inicio=('5:00');
+        $manha_fim=('11:59');
+
+        $tarde_inicio=('12:00');
+        $tarde_fim=('17:59');
+
+        if($agora > $noite_inicio && $agora < $noite_fim){
+            echo "boa Noite";
+        } else if($agora < $manha_fim && $manha_inicio > $agora ){
+            echo "bom dia";
+        }else if($agora > $tarde_inicio && $agora < $tarde_fim){
+            echo 'boa tarde';
+        }
+
 
     }
 }
